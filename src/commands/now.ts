97a151532a738472f07d9a14f84941f7b52833fb
Furkan <furkan@pankod.com>
2019-09-30 11:17:15 +0300

    Formatted now.ts

--- src/commands/now.ts
+++ src/commands/now.ts
@@ -1,25 +1,27 @@
-import {Command, flags} from '@oclif/command'
+import { Command, flags } from "@oclif/command";
 
 export default class Now extends Command {
-  static description = 'describe the command here'
+  static description = "describe the command here";
 
   static flags = {
-    help: flags.help({char: 'h'}),
+    help: flags.help({ char: "h" }),
     // flag with a value (-n, --name=VALUE)
-    name: flags.string({char: 'n', description: 'name to print'}),
+    name: flags.string({ char: "n", description: "name to print" }),
     // flag with no value (-f, --force)
-    force: flags.boolean({char: 'f'}),
-  }
+    force: flags.boolean({ char: "f" })
+  };
 
-  static args = [{name: 'file'}]
+  static args = [{ name: "file" }];
 
   async run() {
-    const {args, flags} = this.parse(Now)
+    const { args, flags } = this.parse(Now);
 
-    const name = flags.name || 'world'
-    this.log(`hello ${name} from /Users/furkan/Workspace/_play/oclif-tryout/src/commands/now.ts`)
+    const name = flags.name || "world";
+    this.log(
+      `hello ${name} from /Users/furkan/Workspace/_play/oclif-tryout/src/commands/now.ts`
+    );
     if (args.file && flags.force) {
-      this.log(`you input --force and --file: ${args.file}`)
+      this.log(`you input --force and --file: ${args.file}`);
     }
   }
 }
