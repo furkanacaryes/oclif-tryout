5a83633391193d1af14b829bf5bc90ce7384423b
Furkan <furkan@pankod.com>
2019-10-01 10:54:42 +0300

    Updated description.

--- src/commands/now.ts
+++ src/commands/now.ts
@@ -1,12 +1,19 @@
 import { Command, flags } from "@oclif/command";
 
 export default class Now extends Command {
-  static description = "you really know what time is it, right?";
+  static description = "You really know what time is it, right?";
 
   static flags = {
     help: flags.help({ char: "h" }),
     // flag with a value (-n, --name=VALUE)
-    name: flags.string({ char: "n", description: "name to print" }),
+    name: flags.string({
+      char: "n",
+      description: "name to print",
+      options: [
+        'this',
+        'that'
+      ]
+    }),
     // flag with no value (-f, --force)
     force: flags.boolean({ char: "f" })
   };
